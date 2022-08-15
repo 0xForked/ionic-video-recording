fd464798b9b2803893df7bd9caee5c0894ccce65
A. A. Sumitro <hello@aasumitro.id>
2022-08-15 12:31:32 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'video-recording',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
